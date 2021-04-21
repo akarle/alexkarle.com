f1e3c604e95a1cd2150a792fbe1756939c3d5898
Alex Karle <alex@alexkarle.com>
2021-04-21 00:31:23 -0400

    jam-tuesday: Add rollup counts to stats.sh
    I'm having way too much fun here... :)

--- jam-tuesday/stats.sh
+++ jam-tuesday/stats.sh
@@ -1,21 +1,46 @@
 #!/bin/sh
 # stats.sh -- get frequencies of jam-tuesday songs/artists
 set -e
+
+N=${1:-10}
 DIR=$(dirname "$0")
 TMP=$(mktemp)
 for set in "$DIR"/[01][0-9]-*; do
     # Remove leading notes, blank lines, and instrument/reprise
     # comments (and blank lines before or after the comments)
-    sed '1,/---/d' "$set" | grep -v '^ *$' | sed 's/ *([^)]*) *//'
+    sed '1,/---/d' "$set" | grep -v '^ *$' | sed 's/ *([^)]*) *//g'
 done > "$TMP"
 
-echo "Top 10 Artists (Frequency, Name):"
+artists() {
+    sed 's/.*, *//' "$TMP" | sort -f | uniq -i -c
+}
+
+songs() {
+    sort -f "$TMP" | uniq -i -c
+}
+
+topN() {
+    sort -n -r | head -n "$N"
+}
+
+count() {
+    wc -l | cut -d' ' -f 1
+}
+
+echo "Play Stats:"
+echo "-----------"
+printf "%7d Songs Total\\n" "$(count < "$TMP")"
+printf "%7d Unique Songs\\n" "$(songs | count)"
+printf "%7d Unique Artists\\n\\n" "$(artists | count)"
+
+echo "Top $N Artists (Frequency, Name):"
 echo "---------------------------------"
-sed 's/.*, *//' "$TMP" | sort -f | uniq -i -c | sort -n -r | head -n 10
+artists | topN
 
 echo ""
-echo "Top 10 Songs (Frequency, Name):"
+echo "Top $N Songs (Frequency, Name):"
 echo "-------------------------------"
-sort -f "$TMP" | uniq -i -c | sort -n -r | head -n 10
+songs | topN
+
 
 rm "$TMP"
